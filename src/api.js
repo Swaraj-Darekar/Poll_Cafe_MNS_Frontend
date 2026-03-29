45b509c815d1ab50e99ce987a7ae0d726092c847
Swaraj-Darekar <swarajdarekar3634@gmail.com>
2026-03-29 09:30:30 +0530

    Initial frontend API error

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
-// const API_BASE_URL = 'https://poll-cafe-mns-backend.onrender.com';
+const API_BASE_URL = 'https://poll-cafe-mns-backend.onrender.com';
 
 export const testBackendConnection = async () => {
     try {
